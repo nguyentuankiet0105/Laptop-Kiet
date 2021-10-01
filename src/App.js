c256d2cc8d14150204fbfa9339f4f2efe9e83ff1
Hàn Kiêu <nguyentuankiet0105@gmail.com>
2021-10-02 05:00:13 +0700

    third commit

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import './styles/App.scss';
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 function App() {
   return (
     <div>
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,12 +4,14 @@ import Footer from '../components/layout/Footer';
 import Header from '../components/layout/header/index';
 import ListProducts from '../components/common/BodyDetails/ListProducts';
 import ScrollToTop from 'react-scroll-to-top';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const Home = () => {
   return (
     <div>
-      <ScrollToTop smooth style={{backgroundColor: '#FFDD46'}}/>
+      <ScrollToTop smooth style={{ backgroundColor: '#FFDD46' }} />
       <Header />
+      <MessengerCustomerChat pageId="107216614982680" appId="558119242164319"/>,
       <Body />
       <Footer />
     </div>
